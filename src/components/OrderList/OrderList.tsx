c8870aefd2706325560f8dfaccfcb74525264954
ishutkoff <ihutkoff@yandex.ru>
2023-02-17 10:38:16 +0300

    hot-fix

---
+++ src/components/OrderList/OrderList.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react'
+import { classNames } from '../../libs/classNames'
+import { useAppStore } from '../../store'
+import { CanvasObjectType } from '../../types/types'
+import cls from './OrderList.module.scss'
+
+interface OrderListProps {
+	className?: string
+	removeHandler: (index?: number) => void
+}
+export const OrderList: FC<OrderListProps> = props => {
+	const objectList = useAppStore(state => state.orderList)
+	const currentMonument = useAppStore(state => state.currentMonument)
+	const { className, removeHandler, ...otherProps } = props
+	return (
+		<div
+			className={classNames(cls.root, {}, [className ? className : ''])}
+			{...otherProps}
+		>
+			<h2>Вы выбрали:</h2>
+			<ol className={cls.orderList}>
+				<li className={cls.orderList__item}>Стелла: {currentMonument.name}</li>
+				{objectList.map((object, index) => (
+					<li className={cls.orderList__item} key={object.name + index}>
+						{object.type === CanvasObjectType.img
+							? 'Эскиз - '
+							: 'Надпись (Шрифт) - '}
+						{object.name}
+						<a href='#' onClick={() => removeHandler(index)}>
+							x
+						</a>
+					</li>
+				))}
+			</ol>
+		</div>
+	)
+}
